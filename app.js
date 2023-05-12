b5dc451c4f90a5059318b00cd83f403a4b3368f9
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-05-12 17:16:35 +0100

    added event listener to generate random key on click

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Key generator
-// declare an empty array that will contain the generated key
+// declare the element that will display the generated key
 
-const practiceKey = [];
+let keyHolder = document.getElementById("statement");
 
 // Then you need an array containing all of the possible options that could be passed into practiceKey
 
@@ -32,65 +32,16 @@ function getRandomKey(array) {
 
 // TODO add an event listener to call the getRandomKey when the button is clicked and display the value
 
-button.addEventListener("click", event) {
-  console.log("Random Key");
-};
+let generatorButton = document.querySelector("button");
 
-// TODO 1: Declare & assign variables pointing to the corresponding element(s)
-// statement should be the "statement" div
-// optionButtons should be all the elements within the "options" div
-// explanation should be the "explanation" div
-
-const statement = document.getElementById("statement");
-
-const optionButtons = document.querySelector("#options").children;
-
-const explanation = document.getElementById("explanation");
-
-// TODO 2: Declare & assign a variable called fact
-// Its value should be an object with a statement, true/false answer, and explanation
-
-const fact = {
-  statement: "Tacos are the greatest === true",
-  answer: true,
-  explanation: "Because yes.",
-};
-
-// TODO 3: Set the text of the statement element to the fact's statement
-
-statement.textContent = fact.statement;
-
-// TODO 4: Declare disable & enable functions to set or remove the "disabled" attribute from a given button element
-// disable(button) should set the button element's attribute "disabled" to the value ""
-
-function disable(button) {
-  button.setAttribute("disabled", "");
-}
-
-/* OR const disable = (button) => button.setAttribute("disabled", "");
-       Target the buttons on the page by doing this:
-
-       disable(optionButtons[0]);
-       */
-
-// enable(button) should remove the attribute "disabled" from the button element
-
-function enable(button) {
-  document.querySelector("button").removeAttribute("disabled");
-}
-
-// OR: const enable = (button) => button.removeAttribute("disabled");
-
-// TODO 5: Declare an isCorrect function that compares a guess to the right answer
-// isCorrect(guess) should return true if the guess matches the fact's answer
-
-function isCorrect(guess) {
-  return guess == fact.answer.toString();
-}
+generatorButton.addEventListener("click", (event) => {
+  let practiceKey = getRandomKey(possibleKeys);
+  keyHolder.textContent = practiceKey;
+});
 
 // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
 
-for (let button of optionButtons) {
+/*for (let button of optionButtons) {
   button.addEventListener("click", (event) => {
     // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
 
@@ -114,4 +65,4 @@ for (let button of optionButtons) {
       button.classList.add("incorrect");
     }
   });
-}
+} */
